d281b1821155883a4432a32ac6367b57c761e7fd
Jordan <jnbarringer96@gmail.com>
2023-02-06 15:29:38 -0600

    globals tutorial done

---
+++ 2-globals.js
@@ -0,0 +1,13 @@
+// GLOBALS - NO WINDOW!!
+// __dirname - path to current directory
+console.log(`console.log(__durname):` + __dirname);
+// __filename - file name
+console.log(`console.log(__filename):` + __filename);
+// require - function to use modules (CommonJS)
+console.log(`console.log(require):` + require);
+// module - info about current module (file)
+console.log(`console.log(module):` + module);
+// process - info about env where the program is being executed
+console.log(`console.log(env):` + env);
+
+//open in integrated terminal and run `node 2-globals.js` or `node 2-globals`
