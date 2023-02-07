c8e94ea643d73a49ee8230baad30bf93ce2303be
Jordan <jnbarringer96@gmail.com>
2023-02-07 15:54:18 -0600

    built in module os tutorial

--- 8-built-in-module-os.js
+++ 8-built-in-module-os.js
@@ -1,1 +1,18 @@
 //OS: Operating System
+const os = require('os');
+
+//info about current user
+const user = os.userInfo();
+console.log(user)
+
+//method returns the system uptime in seconds
+console.log(`The system uptime is ${os.uptime} seconds`);
+
+const currentOS = {
+    name: os.type(),
+    release:os.release(),
+    totalMem: os.totalmem(),
+    freeMem: os.freemem()
+}
+
+console.log(currentOS);
